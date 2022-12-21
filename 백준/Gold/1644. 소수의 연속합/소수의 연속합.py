a38b94d6f73e960859e9940544e1367c766cbfbc
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2022-12-21 21:29:33 +0900

    [Gold III] Title: 소수의 연속합, Time: 6112 ms, Memory: 135228 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+# 소수를 순서대로 정렬해서 prefix sum 을 만들고
+# i ~ j 까지 합이 N 인경우 면 되지않냐?
+# 투포인터? i ~ j 까지 합이 N
+
+prime = []
+
+def is_prime(x):
+    if x <= 1 :
+        return False
+    if x == 2:
+        return True
+    for i in range(2,int(x**0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+for i in range(2,n + 1):
+    if is_prime(i):
+        prime.append(i)
+
+en = 0
+s = 0
+ans = 0
+for i in range(len(prime)):
+    while en < len(prime) and s < n:
+        s += prime[en]
+        en += 1
+    if s == n:
+        ans += 1
+    s -= prime[i]
+print(ans)
