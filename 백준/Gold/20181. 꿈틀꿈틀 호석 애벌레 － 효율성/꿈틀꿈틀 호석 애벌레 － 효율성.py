49eb71ce4f7ca3a33d8480a77e2ec48aae6cffad
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2022-12-26 16:56:16 +0900

    [Gold II] Title: 꿈틀꿈틀 호석 애벌레 - 효율성, Time: 128 ms, Memory: 41216 KB -BaekjoonHub

--- 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.py
+++ 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.py
@@ -12,15 +12,11 @@ def sol(larva,n,k):
     dp = [0]*n
     temp = 0
     for sn in range(n):
-        # 항상 sn <= en 만족하게 만들기
-        if en < sn :
-            en = sn
         # 합 s 가 k 보다 작고, 인덱스범위 안에있다면
         while s < k and en < n:
             s += larva[en]
             en += 1
         dp[en - 1] = max(dp[en - 1],temp + s - k)
-        
         temp = max(dp[sn],temp)
         # sn 을 한칸 증가시킬작업
         s -= larva[sn]
