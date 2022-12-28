a7a7d0219c32620adaa3f532b07c2d43030c27e6
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2022-12-29 01:29:34 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,16 @@
+//
+// Created by 전재현 on 2022/12/29.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum = 0;
+    int score ;
+    for (int i = 0 ; i < 5; i++){
+        cin >> score;
+        sum += score;
+
+    }
+    cout << sum ;
+}
