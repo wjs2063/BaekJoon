0ce81bd869550665d9f40e3cacc574f77a42eb47
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-01-05 00:47:42 +0900

    [Silver II] Title: 회의실 배정 2, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.py
+++ 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.py
@@ -9,27 +9,23 @@ conf = []
 for _ in range(n):
     start,end,p = map(int,input().split())
     conf.append((start,end,p))
-conf.sort()
-answer = 0
 
-def check(cc):
-    #사람수
-    temp = 0
-    # 시작시간
-    t = 0
-    for sn,en,p in cc:
-        if t <= sn:
-            temp += p
-            t = en
-        else:
-            return False,temp
-    return True,temp
+conf.sort(key = lambda x: (x[0],x[1]))
+
+dp = [0]*n
+# 가장 처음에 끝나는 회의
+dp[0] = conf[0][2]
+# dp[i] := 0 ~ i 번째 회의까지  참가가능한 사람의 최댓값
 
 
-for i in range(1,n + 1):
-    ccc = combinations(conf,i)
-    for cc in ccc:
-        flag,temp = check(cc)
-        if flag:
-            answer = max(answer,temp)
-print(answer)
+
+if n >= 2:
+    dp[1] = conf[1][2]
+
+for i in range(2,n):
+    # i 보다 작은값들에 대해서 다 돌아 그래서 dp[i] 를 갱신
+    for j in range(0,i - 1):
+        # i - 2, i - 3 , i - 4  . . . 0 이유는 i번 회의는 i - 1회의랑 안겹친대
+        dp[i] = max(dp[j] + conf[i][2],dp[i])
+#여러 회의가 있을텐데 c1,c2,c3,c4 ~
+print(max(dp))
