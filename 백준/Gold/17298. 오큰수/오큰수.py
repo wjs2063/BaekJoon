fd569f261351517a1462f74965811dd26da1f799
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-02-21 23:51:45 +0900

    [Gold IV] Title: 오큰수, Time: 1624 ms, Memory: 220592 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -4,23 +4,18 @@ input = sys.stdin.readline
 n = int(input())
 
 arr = list(map(int,input().split()))
-# stack 에는 index 를 넣는다
+
 stack = []
 
-dp = [-1]*n
-# 원리를 살펴보면
-
-# stack 에 뭐가 들어있으며 stack 의 최상단에는 이전중 최신 index 가들어있다
-# 이 인덱스에 해당하는 값이 만약 현재살펴볼 arr[i] 보다 작으면
-# 그말 즉 -> 오른쪽중 가장 왼쪽에있는 값이된다 -> arr[i]가
-# 이것을 반복적으로 돌리면서 index에 추가한다
-
-# 일단 stack 에 index 를 추가한다
-for i in range(n):
-    # stack 최상단에 위치한 값이 현재 값 보다 작으면
-    while stack and arr[stack[-1]] < arr[i]:
-        dp[stack.pop()] = arr[i]
-    stack.append(i)
+dp = [-1]*(n)
+# stack 정의
+#
+for i,v in enumerate(arr):
+    # stack 의 최상단에 있는값이 현재값보다 작다면
+    # stack 최상단값의 기준에서는 오큰수가된다.
+    while stack and stack[-1][0] < v:
+        val,idx = stack.pop()
+        dp[idx] = v
+    # 현재값과 idx 를 넣고
+    stack.append([v,i])
 print(*dp)
-
-
