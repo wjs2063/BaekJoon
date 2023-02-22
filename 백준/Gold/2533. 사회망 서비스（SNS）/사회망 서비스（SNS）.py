2cd5eae0522620508050e5ff1b98ed77ac259b39
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-02-22 23:47:18 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 5304 ms, Memory: 345316 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -1,32 +1,35 @@
 import sys
-sys.setrecursionlimit(10**7)
 from collections import defaultdict
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 n = int(input().strip())
-tree = defaultdict(list)
-for _ in range(n - 1):
-    a,b = map(int,input().split())
-    tree[a].append(b)
-    tree[b].append(a)
-INF = int(1e10)
-dp = [[0,1] for _ in range(n + 1)]
-#초기값 세팅
-dp[1][0] = 0
-dp[1][1] = 1
 
-#dp[i][0] -> i 번쨰 노드가 얼리어답터가 아닌경우 최솟값
-#dp[i][1] -> i 번쨰 노드가 얼리어답터 인경우 최솟값
+info = defaultdict(list)
+
+dp = [[0,1] for _ in range(n + 1)]
+
+for _ in range(n - 1):
+    u,v = map(int,input().split())
+    info[u].append(v)
+    info[v].append(u)
+
 
 def dfs(root,parent):
-    # root 가 얼리어답터인경우와 , 얼리어답터가 아닌경우 2가지로 나눠서 진행
-    for child in tree[root]:
+    # 현재 노드가,얼리어답터인경우 dp[root][1]
+    # dp[root][0]
+
+    for child in info[root]:
         if child == parent:continue
-        # root가 얼리어답터 일경우 root가 얼리어답터가 되지않을수도,될수도
         dfs(child,root)
         dp[root][1] += min(dp[child][0],dp[child][1])
-        # root가 얼리어답터가 아닌경우에는 child 는 무조건 얼리어답터여야한다
         dp[root][0] += dp[child][1]
+
 dfs(1,-1)
 
 print(min(dp[1]))
+
+
+
+
+
