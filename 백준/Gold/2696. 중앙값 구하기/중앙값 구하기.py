b6487c2c6c145d5411e9ebc06aa209450fbc222c
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-02-22 16:48:12 +0900

    [Gold II] Title: 중앙값 구하기, Time: 64 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
@@ -2,33 +2,32 @@ import sys
 import heapq
 input = sys.stdin.readline
 
-t = int(input())
+t = int(input().strip())
 
 for _ in range(t):
-    m = int(input().strip())
+    n = int(input().strip())
     arr = []
-    for _ in range(m // 10 + 1):
+    left, right = [], []
+    for _ in range(n // 10 + 1):
         arr.extend(list(map(int,input().split())))
-    # 힙
-    # left 에는 최대힙 ,right 에는 최소힙
-    # 항상 left <= right 으로 유지하되 최대 1개차이만 나도록 구성한다
-    left, right = [] , []
-    res = []
+    # 항상 길이를 left <= right 을 유지하면서
+    # 모든 수들도 left <= right 을 유지하게 해보자
+    # 길이차이는 항상 1이나게 할것이다
+    # 중앙값은 항상 -left[0] 이 되도록 유지한다.
+    answer = []
     for i in range(len(arr)):
-        if len(left) == len(right):
-            heapq.heappush(left,-arr[i])
+        if len(left) <= len(right):
+            heapq.heappush(left, -arr[i])
         else:
             heapq.heappush(right,arr[i])
-
-        # 항상 left <= right 이 유지되게 증 이으면 정렬이 되게유지
-        if left and right and -left[0] > right[0]:
-            x = -heapq.heappop(left)
-            y = heapq.heappop(right)
-            # 최대힙이니까 부호신경써주고
-            heapq.heappush(left,-y)
+        if left and right and  -left[0] > right[0]:
+            x,y = heapq.heappop(left),heapq.heappop(right)
+            x = -x
             heapq.heappush(right,x)
-        # 만약 홀수개라면
-        if i % 2 == 0:
-            res.append(-left[0])
-    print(len(res))
-    print(*res)
+            heapq.heappush(left,-y)
+        if left and i % 2 == 0:
+            answer.append(-left[0])
+    print(len(answer))
+    for i in range(len(answer) // 10 + 1):
+        print(*answer[10*i:10*i + 10])
+
