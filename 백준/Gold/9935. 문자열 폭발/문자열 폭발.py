f76022fd5ae82ae78eab63916e7f29fb15bfd864
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-02-22 10:57:03 +0900

    [Gold IV] Title: 문자열 폭발, Time: 712 ms, Memory: 42436 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,21 +1,22 @@
-import re
-from collections import deque
-my_str=input().rstrip()
-explosion_str=input().rstrip()
+import sys
+input = sys.stdin.readline
 
-def solution2(my_str,explosion_str):
-    last=explosion_str[-1]
-    stack=list()
-    l=len(explosion_str)
-    for strs in my_str:
-        # 문자추가
-        stack.append(strs)
-        if strs==last and "".join(stack[-l:])==explosion_str:
-            #삭제
-            del stack[-l:]
-    answer="".join(stack)
-    if answer=="":
-        print("FRULA")
-    else :
-        print(answer)
-solution2(my_str,explosion_str)
+stack = []
+
+# stack에 하나씩넣으면서 문자열 있으면 계속 지워주기
+
+s = input().strip()
+
+exp_str = list(input().strip())
+
+# O(NM)
+for i in range(len(s)):
+    stack.append(s[i])
+    while len(stack) >= len(exp_str)  and stack[-len(exp_str):] == exp_str:
+        del stack[-len(exp_str):]
+
+
+if stack:
+    print("".join(stack))
+else:
+    print("FRULA")
