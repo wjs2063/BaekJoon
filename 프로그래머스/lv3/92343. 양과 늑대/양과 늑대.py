8ed53cbf9481cfafb2d39be9429f7bb48c3536e2
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-03-01 00:35:05 +0900

    [level 3] Title: 양과 늑대, Time: 0.19 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
@@ -1,51 +1,34 @@
-
-
-        
-
-
+from collections import defaultdict
+vis = [0] * (1 << 17) # 상태 x 방문
 def solution(info, edges):
     answer = 0
-    # bit mask 
-    visited = [0] * (1 << 17)
     n = len(info)
-    # make tree 
-    tree = [[-1,-1] for _ in range(n)]
+    # 0은 양, 늑대는 1 
+    # info[x] : x 번째 노드에 들어있는 동물 
+    # edges[i] = [부모노드,자식노드]
+    tree = defaultdict(list)
     for p,c in edges:
-        if tree[p][0] == -1: tree[p][0] = c
-        else:tree[p][1] = c
-        
-    val = info
+        # 부모노드 ,자식노드
+        tree[p].append(c)
+    # root 노드는 항상 0
     def sol(state):
-        nonlocal visited
+        if vis[state] :return
+        vis[state] = 1
+        w,s = 0,0
+        # 현재 상태에서 얻은 양,늑대 개수를 카운트
+        for i in range(n):
+            if state & ( 1 << i):
+                if info[i] == 0:
+                    s += 1
+                else:
+                    w += 1
+        if w >= s:return
         nonlocal answer
-        nonlocal tree
-        nonlocal val
-        # 상태를 방문했었더라면?
-        if visited[state] : return 
-        # 방문체크
-        visited[state] = 1
-        wolf,num = 0,0
-        # 양은 0 늑대는 1 
+        answer = max(answer,s)
         for i in range(n):
-            # 현재 상태의 늑대 수 구하기 -> 즉 현재 방문노드의 늑대 마리수 체크 (state & ( 1 << i))
-            if state & (1 << i):
-                num += 1
-                wolf += val[i]
-        # 정점 = 양의수 + 늑대수
-        if 2*wolf >= num : return
-        answer = max(answer, num - wolf)
-        # next step
-        for i in range(n):
-            # 방문하지 않은곳이면 패스하고
-            if not state & ( 1 << i): continue
-            # 방문 한 곳이면 현재 노드를 방문체크하고 다음 스텝으로 넘겨
-            if tree[i][0] != -1:
-                sol(state | (1 << tree[i][0]))
-            if tree[i][1] != -1:
-                sol(state | ( 1 << tree[i][1]))
+            if not state & (1 << i):continue
+            for x in tree[i]:
+                sol(state | 1 << x)
     sol(1)
-            
-                
         
-    
     return answer
