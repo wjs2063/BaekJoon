f3a983b05919913424c2d304cc454f685a76ce98
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-03-01 20:56:08 +0900

    [Gold II] Title: 가운데를 말해요, Time: 260 ms, Memory: 44980 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -9,22 +9,25 @@ for _ in range(n):
     arr.append(int(input().strip()))
 
 # 왼쪽에는 최대힙 오른쪽에는 최소힙
-# 항상 l >= r 을 유지한다 -> 길이차이는 최대 1이다
+# 항상 길이는 l >= r 을 유지한다 -> 길이차이는 최대 1이다
 left, right = [], []
 
 res = []
 
 for i in range(n):
+    # 길이가 같으면 left 에
     if len(left) == len(right):
         heapq.heappush(left,-arr[i])
     else:
+    # 다르면 오른쪽에
         heapq.heappush(right,arr[i])
     # left 원소 <= right 원소를 만족해야하므로
-    if left and right and -left[0] >= right[0]:
+    if left and right and -left[0] > right[0]:
         x = -heapq.heappop(left)
         y = heapq.heappop(right)
         heapq.heappush(left,-y)
         heapq.heappush(right,x)
+    # 항상 left[0] -> 중앙값으로 유지
     res.append(-left[0])
 for i in range(len(res)):
     print(res[i])
