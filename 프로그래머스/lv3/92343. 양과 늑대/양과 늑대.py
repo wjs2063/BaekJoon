56629ef20fbb4b4103d2812d2833b8d0cc6e6c7d
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-04-07 18:12:10 +0900

    [level 3] Title: 양과 늑대, Time: 0.19 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
@@ -1,34 +1,28 @@
-from collections import defaultdict
-vis = [0] * (1 << 17) # 상태 x 방문
 def solution(info, edges):
-    answer = 0
     n = len(info)
-    # 0은 양, 늑대는 1 
-    # info[x] : x 번째 노드에 들어있는 동물 
-    # edges[i] = [부모노드,자식노드]
-    tree = defaultdict(list)
+    grp = [[] for _ in range(n)]
     for p,c in edges:
-        # 부모노드 ,자식노드
-        tree[p].append(c)
-    # root 노드는 항상 0
-    def sol(state):
-        if vis[state] :return
-        vis[state] = 1
-        w,s = 0,0
-        # 현재 상태에서 얻은 양,늑대 개수를 카운트
+        grp[p].append(c)
+    
+    answer = 0
+    seen = [0]*(1 << n)
+    def dfs(state):
+        nonlocal answer
+        if seen[state] :return
+        seen[state] = 1
+        s,w = 0,0
         for i in range(n):
-            if state & ( 1 << i):
+            if state & (1 << i):
                 if info[i] == 0:
                     s += 1
                 else:
                     w += 1
-        if w >= s:return
-        nonlocal answer
+        if w >= s :return
         answer = max(answer,s)
         for i in range(n):
-            if not state & (1 << i):continue
-            for x in tree[i]:
-                sol(state | 1 << x)
-    sol(1)
+            if not state & ( 1 << i):continue
+            for x in grp[i]:
+                dfs(state | ( 1 << x))
+    dfs(1)
         
     return answer
