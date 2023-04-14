d161181997b9ecd332278435ad4387b9a7952822
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-04-14 13:22:58 +0900

    [unrated] Title: 요격 시스템, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,4 +1,5 @@
 def solution(targets):
+    """
     targets.sort(key = lambda x:x[1])
     idx = 0
     n = len(targets)
@@ -9,6 +10,24 @@ def solution(targets):
         while idx < n and targets[idx][0] < e:
             idx += 1
         ans += 1
+    """
+    ans = 0
+    line = []
+    for idx,(s,e) in enumerate(targets):
+        line.append([s,e,idx,0])
+        line.append([e,s,idx,1])
+    line.sort()
+    s = set()
+    for i,v in enumerate(line):
+        sn,en,idx,op = v 
+        if op == 0:
+            s.add(idx)
+        elif op == 1:
+            if idx in s:
+                ans += 1
+                s.clear()
+                
+        
     
     return ans 
             
