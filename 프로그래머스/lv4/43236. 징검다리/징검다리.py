347d90e154f65194d7e58cc40ade53eea70dc7e8
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2023-05-12 22:02:19 +0900

    [level 4] Title: 징검다리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv4/43236. 징검다리/징검다리.py
+++ 프로그래머스/lv4/43236. 징검다리/징검다리.py
@@ -1,27 +1,27 @@
 def solution(distance, rocks, n):
     ans = 0
-    rocks = [0] + rocks
     rocks.sort()
-    
-    sn,en = 0,distance
-    
-    while sn <= en:
+    sn,en = 1,distance
+    # 모든 거리를 mid 이상 띄울수있냐?
+    while sn <= en :
         mid = (sn + en) // 2
-        cnt = 0
-        t = 0
-        for i in range(len(rocks) - 1):
-            cnt += abs(rocks[i] - rocks[i + 1])
-            # 돌제거
-            if cnt < mid:
-                t += 1
-            else:
-                cnt = 0
-        if t > n:
-            en = mid - 1
-        else:
-            ans = mid
-            sn = mid + 1
-            
-                
         
+        dist = 0
+        cnt = 0
+        pos = 0
+        for i in range(len(rocks) ):
+            dist += rocks[i] - pos
+            
+            if dist < mid :
+                cnt += 1
+            else:
+                dist = 0
+            pos = rocks[i]
+        if cnt <= n:
+            ans = mid 
+            sn = mid + 1
+        else:
+            en = mid - 1
+                
+    
     return ans
