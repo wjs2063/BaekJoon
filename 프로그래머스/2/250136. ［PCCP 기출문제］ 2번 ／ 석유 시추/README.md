b170b1f60a95015570b469874e2205ffcf894722
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2024-03-08 23:22:44 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 78.62 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
@@ -1,6 +1,6 @@
 # [level 2] [PCCP 기출문제] 2번 / 석유 시추 - 250136 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136?language=python3) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 08일 23:21:54
+2024년 03월 08일 23:22:43
 
 ### 문제 설명
 
--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,36 +1,3 @@
-from collections import deque,defaultdict
-def bfs(land,vis,x,y,cnt,memo):
-    n,m = len(land),len(land[0])
-    q = deque([(x,y,1)])
-    vis[x][y] = cnt
-    memo[cnt] = 1
-    while q:
-        a,b,tot = q.popleft()
-        for na,nb in [(a - 1,b),(a + 1,b),(a,b + 1),(a, b - 1)]:
-            if not (0 <= na < n and 0 <= nb < m):continue
-            if vis[na][nb]:continue 
-            if land[na][nb] == 0 :continue
-            memo[cnt] += 1
-            vis[na][nb] = cnt 
-            q.append((na,nb,tot + 1))
-
 def solution(land):
-    n,m = len(land),len(land[0])
-    cnt = 0
-    vis = [[0] * m for _ in range(n)]
-    memo = defaultdict(int)
-    for i in range(n):
-        for j in range(m):
-            if land[i][j] == 1 and vis[i][j] == 0:
-                cnt += 1
-                bfs(land,vis,i,j,cnt,memo)
     answer = 0
-    for c in range(m):
-        temp = 0
-        v = set()
-        for r in range(n):
-            if land[r][c] == 1 and vis[r][c] not in v:
-                v.add(vis[r][c])
-                temp += memo[vis[r][c]]
-        answer = max(answer,temp)
     return answer
